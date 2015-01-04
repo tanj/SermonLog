8f8663b9b74fc526b50d2d5dab111b7e790376c2
John teBokkel <tanj.tanj@gmail.com>
2015-01-03 19:17:18 -0500

    Begin work on edit_presenter view

--- sermonlog/__init__.py
+++ sermonlog/__init__.py
@@ -18,5 +18,6 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('presenters', '/presenters')
+    config.add_route('edit_presenter', '/presenter/{ixPresenter:\d+}')
     config.scan()
     return config.make_wsgi_app()
--- sermonlog/views/presenters.py
+++ sermonlog/views/presenters.py
@@ -6,6 +6,7 @@ from sqlalchemy.exc import DBAPIError
 from ..models import (
     DBSession,
     TPresenter,
+    TTitle,
     )
 
 @view_config(route_name='presenters', renderer='sermonlog:templates/presenters.jinja2')
@@ -14,4 +15,32 @@ def presenters(request):
         pres = DBSession.query(TPresenter).all()
     except DBAPIError:
         return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'presenters' : pres}
+    return {'presenters' : pres, 'title' : 'Presenter List'}
+
+@view_config(route_name='edit_presenter', renderer='sermonlog:templates/edit_presenter.jinja2')
+def edit_presenter(request):
+    try:
+        presenter = DBSession.query(TPresenter).filter(
+            TPresenter.ixPresenter == int(request.matchdict['ixPresenter'])).one()
+    except DBAPIError:
+        return Response(conn_err_msg, content_type='text/plain', status_int=500)
+    return {'presenter' : presenter, 'title' : 'Edit Presenter'}
+
+
+
+
+conn_err_msg = """\
+Pyramid is having a problem using your SQL database.  The problem
+might be caused by one of the following things:
+
+1.  You may need to run the "initialize_SermonLog_db" script
+    to initialize your database tables.  Check your virtual
+    environment's "bin" directory for this script and try to run it.
+
+2.  Your database server may not be running.  Check that the
+    database server referred to by the "sqlalchemy.url" setting in
+    your "development.ini" file is running.
+
+After you fix the problem, please restart the Pyramid application to
+try it again.
+"""
