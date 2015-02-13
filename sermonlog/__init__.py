104be60fe67f53ab61ac928096434c06a06df83e
John teBokkel <tanj.tanj@gmail.com>
2015-02-13 10:18:33 -0500

    can't include in the package

--- sermonlog/__init__.py
+++ sermonlog/__init__.py
@@ -16,7 +16,7 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     # pyramid_formalchemy's configuration
-    config.include('formalchemy')
+    # config.include('formalchemy')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('presenters', '/presenters')
